1ef39d7c27a24561d3b9955641b862c0cdb5cd54
robin nganga smith <saintofcookies.2021@gmail.com>
2023-11-30 11:33:31 +0300

    Completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,40 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l=[1, 2, 3, 4]
+    l.append(5)
+    return l
+    
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l=[ 1, 2, 3, 4]
+    l.insert(0, 0)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    l=[1, 2, 3, 4]
+    l.pop(3)
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    l=[1, 2, 3, 4]
+    l.pop(0)
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+    l=[1, 2, 3, 4]
+    
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    l=[1, 2, 3, 4]
+    
+    return l[-1]
